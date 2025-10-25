c60f581c2496ace8bef8d4363ce7ac7ea4198b0b
Timothy samuel <128737215+eyitayo77@users.noreply.github.com>
2025-10-25 12:57:54 +0100

    Thanos reads response properly

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('strings/', views.strings_view, name='strings_view'),  
-    path('strings/filter-by-natural-language/', views.filter_nl, name='filter_nl'),  
-    path('strings/<str:string_value>/', views.get_or_delete_string, name='get_or_delete_string'),  
+    path('strings', views.strings_view, name='strings_view'),
+    path('strings/filter-by-natural-language', views.filter_nl, name='filter_nl'),
+    path('strings/<str:string_value>', views.get_or_delete_string,name='get_or_delete_string'),
 ]
-
-
